0ba6ec030de21bfd533f7061fb22652d774ec6bf
CreativezCollection <creativez.collection@gmail.com>
2022-07-14 21:20:44 +0800

    Mobile Testing

--- src/App.js
+++ src/App.js
@@ -12,48 +12,48 @@ import {useMoralis} from "react-moralis"
 
 function App() {
 
-  const {logout} = useMoralis();
+  // const {logout} = useMoralis();
   
 
-  useEffect(() => {
-    if(window.ethereum) {
-      window.ethereum.on('chainChanged', () => {
-        window.location.reload();
-      })
-      window.ethereum.on('accountsChanged', async () => {
-        logout();
-        window.location.reload();
-      })
-  }});
+  // useEffect(() => {
+  //   if(window.ethereum) {
+  //     window.ethereum.on('chainChanged', () => {
+  //       window.location.reload();
+  //     })
+  //     window.ethereum.on('accountsChanged', async () => {
+  //       logout();
+  //       window.location.reload();
+  //     })
+  // }});
 
-  const provider = window.ethereum;
-  if(!provider){
-    window.confirm("Metamask is not installed. Please install and connect wallet to use this DApp")
-  }
+  // const provider = window.ethereum;
+  // if(!provider){
+  //   window.confirm("Metamask is not installed. Please install and connect wallet to use this DApp")
+  // }
 
-  useEffect(() => {
-  async function changeNetwork () {
-  const chainId = await provider.request({ method: "eth_chainId" });
-  const polygonChainId = '0x89';
-  if(chainId === polygonChainId){
-    console.log("Bravo!, you are on the correct network");
-    }else{
-      try {
-        await provider.request({
-          method: 'wallet_switchEthereumChain',
-          params: [{ chainId: polygonChainId}],
-        });
-      console.log("You have succefully switched to Polygon network")
-      } catch (switchError) {
-        // This error code indicates that the chain has not been added to MetaMask.
-        if (switchError.code === 4902) {
-         console.log("This network is not available in your metamask, please add it")
-      }
-        console.log("Failed to switch to the network")
-      }}}
+  // useEffect(() => {
+  // async function changeNetwork () {
+  // const chainId = await provider.request({ method: "eth_chainId" });
+  // const polygonChainId = '0x89';
+  // if(chainId === polygonChainId){
+  //   console.log("Bravo!, you are on the correct network");
+  //   }else{
+  //     try {
+  //       await provider.request({
+  //         method: 'wallet_switchEthereumChain',
+  //         params: [{ chainId: polygonChainId}],
+  //       });
+  //     console.log("You have succefully switched to Polygon network")
+  //     } catch (switchError) {
+  //       // This error code indicates that the chain has not been added to MetaMask.
+  //       if (switchError.code === 4902) {
+  //        console.log("This network is not available in your metamask, please add it")
+  //     }
+  //       console.log("Failed to switch to the network")
+  //     }}}
 
-    changeNetwork(); 
-  }, []);
+  //   changeNetwork(); 
+  // }, []);
 
 
   return (
